67e586963fd5accccbb696ae6abde0a17bf7de67
Raul Fernandez <raulfernandez@client52-10-192-122.eduroam.upc.edu>
2019-04-02 11:59:23 +0200

    GETPID I ALGO MAS DE FORK

--- E1/sys.c
+++ E1/sys.c
@@ -41,10 +41,11 @@ extern int zeos_ticks;
 int sys_gettime() {
 	return zeos_ticks;
 }
+int PID_counter = 1000;
 int sys_fork()
 {
   int PID=-1;
-  if(list_empty(&free_queue)) return -12;
+  if(list_empty(&free_queue)) return -12; // EMONEM Not enough space 
   struct list_head *hijo = list_first(&free_queue);
   list_del(hijo);
   struct task_struct *fill_struct = list_head_to_task_struct(hijo);
@@ -56,8 +57,39 @@ int sys_fork()
   int vframe[NUM_PAG_DATA];
   for(int i = 0; i< NUM_PAG_DATA, ++i) {
     vframe[i] = alloc_frame();
+    if (vframe[i] == -1) {
+      for (int j = 0, j < i; ++j) {
+        free_frame(vframe[j]);
+      }
+      list_add(hijo,&free_queue);
+      return -12; // EMONEM Not enough space 
+    }
   }
-  if (page == -1) 
+  for (int i = 0; i < NUM_PAG_DATA; ++i) {
+    set_ss_page(fill_page_table, PAG_LOG_INIT_DATA+i,vframe[i]);
+  }
+  page_table_entry *pare_page_table = get_PT(current());
+  for (int i=0; i<NUM_PAG_KERNEL; i++){
+		set_ss_pag(fill_page_table, i, get_frame(pare_page_table, i));
+	}
+  for(int i=0; i<NUM_PAG_CODE; i++){
+		//Copiem CODE a child
+		set_ss_pag(fill_page_table, PAG_LOG_INIT_CODE+i, get_frame(pare_page_table, PAG_LOG_INIT_CODE+i));
+	}
+  int first_data_page = NUM_PAG_KERNEL + NUM_PAG_CODE;
+	int first_free_page = first_data_page + NUM_PAG_DATA;
+  for (int i = first_data_page; i<first_free_page; i++) {
+    set_ss_pag(parent_page_table, first_free_page, get_frame(child_page_table, i));
+    copy_data((void*)((first_data_page + i) * PAGE_SIZE), (void*)((first_free_page + i) * PAGE_SIZE), PAGE_SIZE);
+    del_ss_pag(parent_page_table, first_free_page+i);
+  }
+  set_cr3(get_DIR(current()));
+  child_task_struct->PID = ++PID_counter;
+  int ebp;
+  get_ebp(ebp);
+  ebp = ebp - ((int)current()); //Diferencia entre ESP i current del pare i despres li sumem al current del fill
+	ebp += (int)(child_task_union); //ens posicionem on començen les dades del fill, que es el ESP del fill
+	child_task_union->task.esp = ebp; 
 
   return PID;
 }
